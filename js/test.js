2ccc79ed24244f041c858cea4cfe0e151b54ebc9
Michail <Gismanus@gmail.com>
2022-10-26 18:39:20 +0300

    destructurisation

--- js/test.js
+++ js/test.js
@@ -1,1 +1,22 @@
-console.log('Hello');
+let salaries = {
+    'John': 100,
+    'Pete': 300,
+    'Mary': 250,
+}
+
+function topSalary(obj) {
+    let max = 0;
+    let maxName = null;
+
+    for (const [name, salary] of Object.entries(obj)) {
+        if(max < salary){
+            max = salary;
+            maxName = name;
+        }
+        
+    }
+    return maxName;
+}
+
+console.log(topSalary({}));
+console.log(topSalary(salaries));
